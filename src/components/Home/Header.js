870ff8693081071d57efe623a3ec5ee2b38a8cfb
Mohamed Abdallah <53374376+mohamed17-a@users.noreply.github.com>
2024-02-19 08:04:50 +0200

    Update Header.js

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
                         </p>
                         <a href='https://www.linkedin.com/in/mohamedabdallah17/' target='blank'> <FontAwesomeIcon icon={faLinkedin} /></a>
                         <a href='https://github.com/mohamed17-a' target='blank'><FontAwesomeIcon icon={faGithub} /></a>
-                        <Button id="resumeBtn" href='https://drive.google.com/file/d/11LnrjYH8Syf7FP_tDppkXvQySw6U5_9H/view?usp=sharing' target='blank'>Resume <FontAwesomeIcon icon={faDownload} /></Button>
+                        <Button id="resumeBtn" href='https://drive.google.com/file/d/1U0FtvaxAM0e6WCsFaB46xonFNyjwE_Kf/view?usp=sharing' target='blank'>Resume <FontAwesomeIcon icon={faDownload} /></Button>
                     </div>
                     <div className='col-md-4 imgdiv'>
                         <img src={character} alt='My Character' />

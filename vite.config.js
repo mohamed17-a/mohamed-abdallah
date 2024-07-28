614fb214db9e8eb096a43003e73bbb6549cadffe
Mohamed Abdallah <53374376+mohamed17-a@users.noreply.github.com>
2024-07-29 00:00:03 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/src',
   build: {
     outDir: 'dist', // Ensure the output directory is 'dist'
   },
